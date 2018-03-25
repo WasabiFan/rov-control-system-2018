3a8ba57fe72364e08aaaefec650452a0c89acc28
Kaelin Laundry <wasabifan@outlook.com>
2018-03-25 00:22:20 -0700

    Strip out old stuff from view model and UI

--- operator-interface/MainPageViewModel.cs
+++ operator-interface/MainPageViewModel.cs
@@ -15,101 +15,39 @@ namespace RovOperatorInterface
         public enum RovConnectionState
         {
             NotConnected,
-            Connecting,
             Connected
         }
-
-        private string _RovHostName = "monkeyrov";
-        private string _RovServiceName { get; set; } = "5001";
-        private bool _ShouldAttemptConnection { get; set; }
+        
         private bool _IsRovEnabled { get; set; }
         private bool _IsConnectButtonEnabled { get; set; }
-
-        private string _MeanTransitTime { get; set; }
 
         private double _VehiclePitch { get; set; }
         private double _VehicleRoll { get; set; }
 
         private bool _IsImuEnabled { get; set; }
 
-        private RovConnectionState _ConnectionState { get; set; }
+        private bool _IsConnected { get; set; }
 
         public ObservableCollection<string> LogMessages { get; protected set; } = new ObservableCollection<string>();
 
-        public RovConnectionState ConnectionState
+        public bool IsConnected
         {
-            get { return _ConnectionState; }
+            get { return _IsConnected; }
             set
             {
-                _ConnectionState = value;
-                if (PropertyChanged != null)
+                if (value != _IsConnected)
                 {
-                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(ConnectButtonText)));
-                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(IsConnectButtonEnabled)));
-                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(IsEnableButtonEnabled)));
+                    _IsConnected = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsConnected)));
                 }
             }
         }
-
-        public string RovHostName
-        {
-            get { return _RovHostName; }
-            set
-            {
-                if (value != _RovHostName)
-                {
-                    _RovHostName = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(RovHostName)));
-                }
-            }
-        }
-
-        public string RovServiceName
-        {
-            get { return _RovServiceName; }
-            set
-            {
-                if (value != _RovServiceName)
-                {
-                    _RovServiceName = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(RovServiceName)));
-                }
-            }
-        }
-
-        public bool ShouldAttemptConnection
-        {
-            get { return _ShouldAttemptConnection; }
-            set
-            {
-                if (value != _ShouldAttemptConnection)
-                {
-                    _ShouldAttemptConnection = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShouldAttemptConnection)));
-                }
-            }
-        }
-
-        public string ConnectButtonText
-        {
-            get => ConnectionState == RovConnectionState.Connected ? "Connected" :
-                    ConnectionState == RovConnectionState.Connecting ? "Connecting..." :
-                    "Connect";
-        }
-
-        public bool IsConnectButtonEnabled
-        {
-            get
-            {
-                return ConnectionState == RovConnectionState.NotConnected;
-            }
-        }
-
+        
         public bool IsEnableButtonEnabled
         {
             get
             {
-                return ConnectionState == RovConnectionState.Connected;
+                return IsConnected;
             }
         }
 
@@ -122,19 +60,6 @@ namespace RovOperatorInterface
                 {
                     _IsRovEnabled = value;
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRovEnabled)));
-                }
-            }
-        }
-
-        public string MeanTransitTime
-        {
-            get { return _MeanTransitTime; }
-            set
-            {
-                if (value != _MeanTransitTime)
-                {
-                    _MeanTransitTime = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MeanTransitTime)));
                 }
             }
         }
