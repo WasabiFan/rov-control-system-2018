be73c70e3033c1c1cce19628986b8e2d37c5b94d
WasabiFan <wasabifan@outlook.com>
2018-05-18 08:44:28 -0700

    Implementation of multi-tone trigger

--- obs-release/src/main.cpp
+++ obs-release/src/main.cpp
@@ -4,49 +4,97 @@
 
 AudioInputAnalog audioIn(A2);
 
-//AudioAnalyzeFFT1024 fft;
-//AudioConnection patchCord1(audioIn, fft);
-AudioAnalyzeToneDetect toneDetect;
-AudioConnection patchCord1(audioIn, toneDetect);
+AudioAnalyzeFFT1024 fft;
+AudioConnection patchCord1(audioIn, fft);
 
+#define NO_TIME (UINT32_MAX)
+#define TONE_TIME_THRESH 1000
+#define RESET_TIME_THRESH 500
+
+int detectionStage = 0;
+const int frequencyBuckets[] PROGMEM = {19, 23};
+uint32_t lastStageEndTime = NO_TIME;
+uint32_t detectionStartTime = NO_TIME;
 
 void setup()
-{                
-  Serial.begin(115200);
-  while(!Serial);
-  Serial.println("DSFSF");
-    AudioMemory(12);
-    toneDetect.frequency(800, 100);
-  //fft.windowFunction(AudioWindowHanning1024);
+{
+    Serial.begin(115200);
 }
 
-int val;
+void activateTrigger()
+{
+    Serial.println("TRIGGER");
+}
 
 void loop()
 {
-  //adc1.update();
-  //print1.update();
-  //val = analogRead(A2);
-  //Serial.println(val);
-  //delay(5);
+    //adc1.update();
+    //print1.update();
+    //val = analogRead(A2);
+    //Serial.println(val);
+    //delay(5);
 
-  /*float n;
-  int i;
-  if (fft.available()) {
-    // each time new FFT data is available
-    // print it all to the Arduino Serial Monitor
-    Serial.print("FFT: ");
-    for (i=0; i<40; i++) {
-      n = fft.read(i);
-      if (n >= 0.01) {
-        Serial.print(n);
-        Serial.print(" ");
-      } else {
-        Serial.print("   "); // don't print "0.00"
-      }
+    if (fft.available())
+    {
+        uint32_t time = millis();
+        bool isFreqActive = fft.read(frequencyBuckets[detectionStage]) > 0.05;
+        if (isFreqActive)
+        {
+            if (detectionStartTime == NO_TIME)
+            {
+                detectionStartTime = time;
+            }
+            else if (time - detectionStartTime > TONE_TIME_THRESH)
+            {
+                Serial.print("Accepted stage ");
+                Serial.print(detectionStage);
+                Serial.println("; advancing");
+
+                detectionStage++;
+                lastStageEndTime = time;
+                detectionStartTime = NO_TIME;
+
+                if (detectionStage >= sizeof(frequencyBuckets) / sizeof(int))
+                {
+                    activateTrigger();
+                    detectionStage = 0;
+                    lastStageEndTime = NO_TIME;
+                }
+            }
+        }
+        else
+        {
+            detectionStartTime = NO_TIME;
+            if (lastStageEndTime != NO_TIME && time - lastStageEndTime > RESET_TIME_THRESH)
+            {
+                Serial.print("Timed out waiting for stage ");
+                Serial.println(detectionStage);
+                lastStageEndTime = NO_TIME;
+                detectionStage = 0;
+            }
+        }
     }
-    Serial.println();
-  }*/
-  Serial.println(toneDetect.read());
-  delay(10);
+/*
+    float n;
+    int i;
+    if (fft.available())
+    {
+        // each time new FFT data is available
+        // print it all to the Arduino Serial Monitor
+        Serial.print("FFT: ");
+        for (i = 0; i < 40; i++)
+        {
+            n = fft.read(i);
+            if (n >= 0.01)
+            {
+                Serial.print(n);
+                Serial.print(" ");
+            }
+            else
+            {
+                Serial.print("   "); // don't print "0.00"
+            }
+        }
+        Serial.println();
+    }*/
 }
